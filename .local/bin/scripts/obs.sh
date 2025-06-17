1d37a94f8bcb6f787885895f51c2d6195a8d37b0
yahorni <neuralorge@gmail.com>
2025-06-18 02:58:41 +0300

    Remove useless obsidian functions, add gpg decrypt to lf

--- .local/bin/scripts/obs.sh
+++ .local/bin/scripts/obs.sh
@@ -3,12 +3,10 @@ cd "$HOME/dox/notes" || exit 1
 cmd=
 case "$1" in
     "quick") cmd=':ObsidianQuickSwitch' ;;
-    "new") cmd=':ObsidianNew' ;;
     "today") cmd=':ObsidianToday' ;;
-    "tomorrow") cmd=':ObsidianToday +1' ;;
     "yesterday") cmd=':ObsidianToday -1' ;;
+    "tomorrow") cmd=':ObsidianToday +1' ;;
     "search") cmd=':ObsidianSearch' ;;
-    "open") cmd=':ObsidianOpen' ;;
     "ledger") cmd=":e $(find ./*personal/*finance/*ledger/ -maxdepth 1 -name "$(date +%Y)"'.ledger' | head -n1)" ;;
 esac
 exec "$TERMINAL" -e "$EDITOR" -c "$cmd"
